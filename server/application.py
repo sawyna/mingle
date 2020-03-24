9e99096b646fcc2b054d9ee9a71a692229f9e1d3
Yaswanth Kumar <yash@pointy.com>
2020-03-24 22:56:56 +0000

    few server logs

--- server/application.py
+++ server/application.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
@@ -8,6 +9,7 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('client_send')
 def handle_client_send(message):
+    print('Received request from ', flask.request.sid)
     print('Received message ', message)
     payload = message['payload']
     channel_id = payload['channelId']
@@ -15,12 +17,12 @@ def handle_client_send(message):
 
 @socketio.on('client_join')
 def handle_client_join(message):
+    print('Client join: ', flask.request.sid)
     payload = message['payload']
     user_id = payload['userId']
     channel_id = payload['channelId']
     join_room(channel_id)
-    print('User %s joined room %s', user_id, channel_id)
-    send('User %s has entered the room' % user_id, room=channel_id)
+    print('User joined room', user_id, channel_id)
 
 # run the app.
 if __name__ == "__main__":
