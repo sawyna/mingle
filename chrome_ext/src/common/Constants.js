e599386f17c260b3af680cc04aa38a2615900cb8
Yaswanth Kumar <yash@pointy.com>
2020-04-27 20:02:54 +0100

    Add util and constants

---
+++ chrome_ext/src/common/Constants.js
@@ -0,0 +1,6 @@
+// const SERVER_URL = 'http://34.210.140.190:5000';
+const SERVER_URL = 'http://localhost:5000';
+
+export default {
+    SERVER_URL,
+}
---
+++ chrome_ext/src/common/Util.js
@@ -0,0 +1,43 @@
+import lodash from 'lodash-core';
+
+const _MINGLE_ENABLED_HOSTS = [
+    'youtube',
+    'netflix',
+    'amazon',
+    'primevideo',
+];
+
+const getMingleChannel = (url) => {
+    if (lodash.isUndefined(url)) {
+        url = document.URL;
+    }
+    console.log(`getting mingle channel for url: ${url}`);
+    const urlInstance = new URL(url);
+    return urlInstance.searchParams.get('mingleChannelId');
+};
+
+const isMingleActive = () => {
+    return lodash.isNil(getMingleChannel());
+}
+
+const isMingleEnabled = () => {
+    const url = new URL(document.URL);
+    const hostname = url.hostname;
+
+    return _MINGLE_ENABLED_HOSTS.some((host) => host in hostname);
+}
+
+const customSetInterval = (func, timeout, immediate) => {
+    if (immediate) {
+        func();
+    }
+
+    return setInterval(func, timeout);
+}
+
+export default {
+    getMingleChannel,
+    isMingleActive,
+    isMingleEnabled,
+    customSetInterval,
+}
